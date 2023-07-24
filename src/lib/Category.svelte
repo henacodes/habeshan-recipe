e57d9e1f271649ecc260e3af9e8a04dc7441a654
Henok Ayenew <henacodes@gmail.com>
2023-07-24 08:23:57 +0400

    finished routing  and added some styles

--- src/lib/Category.svelte
+++ src/lib/Category.svelte
@@ -1,10 +1,23 @@
 <script>
+  import { onMount } from "svelte";
+  import { page } from "$app/stores";
   export let type;
+
+  let param = "";
+  onMount(() => {
+    const unsubscribe = page.subscribe((value) => {
+      param = value.params.category;
+    });
+    return () => {
+      unsubscribe();
+    };
+  });
 </script>
 
 <a href={`/foods/${type}`} class="flex-grow">
   <div
-    class="category border border-gray-300 rounded p-3 m-2 hover:scale-105 transition duration-300 ease-in-out hover:bg-green-color hover:shadow-2xl cursor-pointer hover:text-white-grey"
+    class={(param == type ? "bg-green-color" : "") +
+      " category  border border-gray-300 rounded p-3 m-2 hover:scale-105 transition duration-300 ease-in-out hover:bg-green-color hover:shadow-2xl cursor-pointer hover:text-white-grey"}
   >
     <img src={`/foods/${type}.png`} width="30px" alt="category" class="" />
     <p class=" ">{type}</p>
