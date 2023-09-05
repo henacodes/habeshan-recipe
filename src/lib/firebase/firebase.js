8dfcd7f4220991ccb9555162fd5eadfaeaed0a66
Henok Ayenew <henacodes@gmail.com>
2023-09-05 09:45:59 +0400

    removed auth routes

---
+++ src/lib/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAx1sXDrIhpRMjQHC3sldCxa_vLapI3b_4",
+  authDomain: "e-kushna.firebaseapp.com",
+  projectId: "e-kushna",
+  storageBucket: "e-kushna.appspot.com",
+  messagingSenderId: "250457257136",
+  appId: "1:250457257136:web:9fd0850d0d1455a73c7775",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
