7f0aa0e3d6be654d08dd8e5be26a0c9e92d12016
Henok Ayenew <henacodes@gmail.com>
2023-09-05 11:24:48 +0400

    google popup sign in done

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -7,22 +7,42 @@
   } from "svelte-feather-icons";
   import { onMount } from "svelte";
   import { page } from "$app/stores";
+  import {
+    signInWithPopup,
+    GoogleAuthProvider,
+    setPersistence,
+    browserLocalPersistence,
+  } from "firebase/auth";
+  import { auth } from "./firebase/firebase.js";
   let route = "";
   const assignClass = (menu, value) => {
     if (menu == value) {
       return "text-green-color font-bold border-r-2 border-green-color ";
     }
   };
-  onMount(() => {
+
+  const handleGoogleAuth = async () => {
+    try {
+      // Set the persistence to local
+      await setPersistence(auth, browserLocalPersistence);
+
+      // Perform Google sign-in
+      const res = await signInWithPopup(auth, new GoogleAuthProvider());
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  onMount(async () => {
     const unsubscribe = page.subscribe((value) => {
       route = value.route.id.split("/")[1];
     });
+    // Check if the user is already authenticated
 
     return () => {
       unsubscribe();
     };
   });
-  console.log(route);
 </script>
 
 <div class="sidebar bg-[#f2f3f5] flex-[0.2]">
@@ -31,9 +51,13 @@
       Mad<span class="text-white-grey">bet</span>
     </p>
     <p class=" text-slate-400">A home for creative moms</p>
-    <button class="bg-green-color p-1 w-full mt-5 mb-1 rounded-sm"
-      >sign in</button
+    <button
+      on:click={handleGoogleAuth}
+      class="bg-[#ffffff] flex items-center rounded-md p-2 mt-5"
     >
+      <img class=" w-[1vw] m-1" src="/google.png" alt="" />
+      <span class="">Sign up with Google</span>
+    </button>
   </div>
   <div class="sidebar-menu p-5">
     <p class="my-5 font-bold text-xl">Menu</p>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,27 @@
 <script>
   import Sidebar from "$lib/Sidebar.svelte";
   import Feedbar from "$lib/Feedbar.svelte";
+  import { authStore } from "../store/authStore.js";
+  import { auth } from "../lib/firebase/firebase.js";
   import "../app.css";
+  import { onMount } from "svelte";
+
+  onMount(async () => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      authStore.update((prev) => {
+        return {
+          ...prev,
+          user: {
+            name: user.displayName,
+            email: user.email,
+            profilePic: user.photoURL,
+          },
+        };
+      });
+    });
+    authStore.subscribe((user) => console.log(user));
+    return unsubscribe;
+  });
 </script>
 
 <div class="app flex w-full h-[100vh]">
