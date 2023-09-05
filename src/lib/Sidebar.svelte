54f23000fb26d07e750a49e182a13a338f0ba050
Henok Ayenew <henacodes@gmail.com>
2023-09-05 12:06:22 +0400

    added logout and some ui stuff

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -4,14 +4,18 @@
     CoffeeIcon,
     PlusSquareIcon,
     InfoIcon,
+    UserIcon,
+    LogOutIcon,
   } from "svelte-feather-icons";
   import { onMount } from "svelte";
   import { page } from "$app/stores";
+  import { authStore } from "../store/authStore.js";
   import {
     signInWithPopup,
     GoogleAuthProvider,
     setPersistence,
     browserLocalPersistence,
+    signOut,
   } from "firebase/auth";
   import { auth } from "./firebase/firebase.js";
   let route = "";
@@ -33,6 +37,15 @@
       console.error(error);
     }
   };
+
+  const handleLogOut = async () => {
+    try {
+      await signOut(auth);
+      window.location.href = "/";
+    } catch (error) {
+      console.log(error);
+    }
+  };
   onMount(async () => {
     const unsubscribe = page.subscribe((value) => {
       route = value.route.id.split("/")[1];
@@ -45,49 +58,88 @@
   });
 </script>
 
-<div class="sidebar bg-[#f2f3f5] flex-[0.2]">
-  <div class="sidebar-top bg-dark-blue p-5">
-    <p class=" text-green-color font-bold text-3xl">
-      Mad<span class="text-white-grey">bet</span>
-    </p>
-    <p class=" text-slate-400">A home for creative moms</p>
-    <button
-      on:click={handleGoogleAuth}
-      class="bg-[#ffffff] flex items-center rounded-md p-2 mt-5"
-    >
-      <img class=" w-[1vw] m-1" src="/google.png" alt="" />
-      <span class="">Sign up with Google</span>
-    </button>
-  </div>
-  <div class="sidebar-menu p-5">
-    <p class="my-5 font-bold text-xl">Menu</p>
-    <div class="menu">
-      <a href="/foods/breakfast"
-        ><div class={"flex items-center my-4 " + assignClass("foods", route)}>
-          <CoffeeIcon size="20" class=" text-dark-blue mr-4" />
-          <span>Foods</span>
-        </div></a
-      >
-      <a href="/favorites">
-        <div
-          class={"flex items-center my-4 " + assignClass("favorites", route)}
+<div class="sidebar bg-[#f2f3f5] flex-[0.2] flex justify-between flex-col">
+  <div>
+    <div class="sidebar-top bg-dark-blue p-5">
+      <p class=" text-green-color font-bold text-3xl">
+        Mad<span class="text-white-grey">bet</span>
+      </p>
+      <p class=" text-slate-400">A home for creative moms</p>
+      {#if !$authStore?.user}
+        <button
+          on:click={handleGoogleAuth}
+          class="bg-[#ffffff] flex items-center rounded-md p-2 mt-5"
         >
-          <HeartIcon size="20" class=" text-dark-blue mr-4" />
-          <span>Favorites</span>
-        </div></a
-      >
-      <a href="/create">
-        <div class={"flex items-center my-4 " + assignClass("create", route)}>
-          <PlusSquareIcon size="20" class=" text-dark-blue mr-4" />
-          <span>Create</span>
-        </div>
-      </a>
-      <a href="/about">
-        <div class={"flex items-center my-4 " + assignClass("about", route)}>
-          <InfoIcon size="20" class=" text-dark-blue mr-4" />
-          <span>About</span>
-        </div></a
-      >
+          <img class=" w-[1vw] m-1" src="/google.png" alt="" />
+          <span class="">Sign up with Google</span>
+        </button>
+      {/if}
+    </div>
+    <div class="sidebar-menu px-5">
+      <p class="my-5 font-bold text-xl">Menu</p>
+      <div class="menu">
+        <a href="/foods/breakfast"
+          ><div class={"flex items-center my-4 " + assignClass("foods", route)}>
+            <CoffeeIcon size="20" class=" text-dark-blue mr-4" />
+            <span>Foods</span>
+          </div></a
+        >
+        <a href="/favorites">
+          <div
+            class={"flex items-center my-4 " + assignClass("favorites", route)}
+          >
+            <HeartIcon size="20" class=" text-dark-blue mr-4" />
+            <span>Favorites</span>
+          </div></a
+        >
+        <a href="/create">
+          <div class={"flex items-center my-4 " + assignClass("create", route)}>
+            <PlusSquareIcon size="20" class=" text-dark-blue mr-4" />
+            <span>Create</span>
+          </div>
+        </a>
+        <a href="/about">
+          <div class={"flex items-center my-4 " + assignClass("about", route)}>
+            <InfoIcon size="20" class=" text-dark-blue mr-4" />
+            <span>About</span>
+          </div></a
+        >
+      </div>
     </div>
   </div>
+
+  {#if $authStore.user}
+    <div class="sidebar-bottom p-5">
+      <div class="group relative">
+        <div
+          class="flex items-center bg-white py-4 px-4 rounded-xl cursor-pointer hover:bg-slate-200 transition duration-300 ease-in-out"
+        >
+          <img
+            src={$authStore.user?.profilePic}
+            alt=""
+            class="w-7 rounded-full"
+          />
+          <p class="ml-2">{$authStore.user?.name}</p>
+        </div>
+        <div
+          class="absolute bottom-full mb-1 left-0 bg-white text-gray-700 py-2 px-3 rounded-md opacity-0 transform scale-0 group-hover:opacity-100 group-hover:scale-100 transition duration-300 ease-in-out"
+        >
+          <!-- Tooltip content -->
+          <p
+            class="flex items-center mt-2 hover:bg-slate-100 p-2 rounded-md cursor-pointer"
+          >
+            <UserIcon size="20" class=" text-dark-blue mr-4" />
+            <span>Profile</span>
+          </p>
+          <p
+            on:click={handleLogOut}
+            class="flex items-center mt-2 hover:bg-slate-100 p-2 rounded-md cursor-pointer"
+          >
+            <LogOutIcon size="20" class=" text-dark-blue mr-4" />
+            <span>Logout</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  {/if}
 </div>
